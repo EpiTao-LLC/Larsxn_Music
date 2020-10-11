74e832dc176356d69f6fc62bab1abf959f515c92
Chris Larson <59679924+clarson10024@users.noreply.github.com>
2020-10-10 21:07:27 -0600

    css background color switch to black

---
+++ .history/src/App_20201010210224.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+// import './App.css';
+import SEO from "./components/seo"
+import titles from "./content_data/titles"
+import Layout from "./components/Layout"
+import ErrorBoundry from "./components/ErrorBoundary"
+
+function App() {
+
+  // Set the context for the app.  The first item in the titles array is the newest work
+  // that will display on the header page.  The main sections (artist message and demos)
+  // will display below the header as artistsPage is set to false (default).  When artistPage
+  // is true the Layout will display the artists who's work is featured.
+
+  const [context, setContext ] = useState({
+    title: titles[0].titleName,
+    artistsPage: false
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ErrorBoundry>
+          <div>
+            <SEO />
+            <Layout
+            appContext= {[context, setContext]}
+            />
+           </div>
+        </ErrorBoundry>
+      </header>
+    </div>
+  );
+}
+
+export default App;
